0c03a550faf9ee9fea6318f629b43a7dbb61ae7c
Renato Dehnhardt <renatotkd@gmail.com>
2025-01-21 08:29:52 +0100

    add possibility to make a class

--- src/Action.php
+++ src/Action.php
@@ -8,16 +8,24 @@ use Illuminate\Support\Facades\DB;
 class Action
 {
   /**
+   * Make a static class
+   */
+  public static function make(): static
+  {
+    return app(static::class);
+  }
+
+  /**
    * Action entry point
    */
   public static function run(mixed ...$arguments): mixed
   {
     if (in_array(Transaction::class, class_uses(static::class))) {
       return DB::transaction(function () use ($arguments) {
-        return app(static::class)->handle(...$arguments);
+        return static::make()->handle(...$arguments);
       });
     }
 
-    return app(static::class)->handle(...$arguments);
+    return static::make()->handle(...$arguments);
   }
 }
--- tests/Unit/ActionTest.php
+++ tests/Unit/ActionTest.php
@@ -1,8 +1,15 @@
 <?php
 
+use Baconfy\Support\Action;
 use Baconfy\Support\Tests\Fake\ConcatenateTwoStrings;
 use Baconfy\Support\Tests\Fake\TransactionalAction;
 use Illuminate\Support\Facades\DB;
+
+it('can make an action without run', function () {
+  $concatenate = ConcatenateTwoStrings::make();
+
+  expect($concatenate)->toBeInstanceOf(Action::class);
+});
 
 it('can extends an Action class', function () {
   $concatenate = ConcatenateTwoStrings::run('Awesome', 'Test');
